8de8f2b02387911f71b5acd3858f8cb49cfaf183
sqshine <>
2018-06-28 17:49:53 +0800

    add gateway edge-service

--- edge-service/src/main/java/com/sqshine/edgeservice/EdgeServiceApplication.java
+++ edge-service/src/main/java/com/sqshine/edgeservice/EdgeServiceApplication.java
@@ -1,10 +1,26 @@
 package com.sqshine.edgeservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+/**
+ * @author sqshine
+ */
+@SpringCloudApplication
 public class EdgeServiceApplication {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("start", p -> p.path("/start")
+                        .uri("http://www.baidu.com"))
+
+                .build();
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(EdgeServiceApplication.class, args);
